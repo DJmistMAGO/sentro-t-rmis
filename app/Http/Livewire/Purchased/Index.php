ffcb8916322f71b334055a1e50d9fc0498f0f7d4
johnkenneth24 <kengerero@yahoo.com>
2023-12-06 01:32:14 +0800

    done export

--- app/Http/Livewire/Purchased/Index.php
+++ app/Http/Livewire/Purchased/Index.php
@@ -10,8 +10,12 @@ use PhpOffice\PhpWord\TemplateProcessor;
 class Index extends Component
 {
 
-    public $dateInput;
+    public $from;
+    public $to;
+    public $selectedDay;
     public $path_export = 'docx/stms.docx';
+
+    
 
     public function render(Request $request)
     {
@@ -43,48 +47,211 @@ class Index extends Component
         $path = storage_path($this->path_export);
         $templateProcessor = new TemplateProcessor($path);
 
-        $purchasedProducts = PurchaseProductInfo::with('purchasedProducts')->where('date_preparation', $this->dateInput)->get();
+        if ($this->selectedDay == "today") {
+            $purchasedProducts = PurchaseProductInfo::with('purchasedProducts')->where('date_preparation', now()->format('Y-m-d'))->get();
 
-        if ($purchasedProducts->isEmpty()) {
-            session()->flash('message', 'No records found for the selected date.');
-        } else {
+            if ($purchasedProducts->isEmpty()) {
+                session()->flash('message', 'No records found for the selected date.');
+            } else {
 
-            $count = $purchasedProducts->count();
+                $count = $purchasedProducts->count();
 
-            if ($count > 0) {
-                $templateProcessor->cloneRow('reference', $count);
+                if ($count > 0) {
+                    $templateProcessor->cloneRow('reference', $count);
+                }
+
+                $totalAll = 0;
+
+                foreach ($purchasedProducts as $key => $transaction) {
+                    $templateProcessor->setValue('reference#' . ($key + 1), $transaction->reference_no);
+                    $templateProcessor->setValue('date#' . ($key + 1), $transaction->date_preparation->format('Y-m-d'));
+                    $total = $transaction->purchasedProducts->sum('total');
+                    $templateProcessor->setValue('total_transaction#' . ($key + 1), number_format($total, 2));
+                    $totalAll += $total;
+                }
+
+                $templateProcessor->setValue('total', number_format($totalAll, 2));
+
+                $templateProcessor->setValue('date', date('Y-m-d'));
+                $templateProcessor->setValue('user', auth()->user()->name);
+
+
+
+                $filename = 'Sales';
+                $tempPath = 'reports/' . $filename . '.docx';
+
+                if (!file_exists(storage_path('reports'))) {
+                    mkdir(storage_path('reports'), 0777, true);
+                }
+
+                $templateProcessor->saveAs(storage_path($tempPath));
+                return response()->download(storage_path($tempPath));
             }
+        } else if ($this->selectedDay == "week") {
+            $purchasedProducts = PurchaseProductInfo::with('purchasedProducts')->whereRaw('WEEK(date_preparation) = ' . date('W'))->get();
 
-            $totalAll = 0;
+            if ($purchasedProducts->isEmpty()) {
+                session()->flash('message', 'No records found for the selected date.');
+            } else {
 
-            foreach ($purchasedProducts as $key => $transaction) {
-                $templateProcessor->setValue('reference#' . ($key + 1), $transaction->reference_no);
+                $count = $purchasedProducts->count();
 
-                $total = $transaction->purchasedProducts->sum('total');
-                $templateProcessor->setValue('total_transaction#' . ($key + 1), number_format($total, 2));
+                if ($count > 0) {
+                    $templateProcessor->cloneRow('reference', $count);
+                }
 
-                $totalAll += $total;
+                $totalAll = 0;
+
+                foreach ($purchasedProducts as $key => $transaction) {
+                    $templateProcessor->setValue('reference#' . ($key + 1), $transaction->reference_no);
+                    $templateProcessor->setValue('date#' . ($key + 1), $transaction->date_preparation->format('Y-m-d'));
+                    $total = $transaction->purchasedProducts->sum('total');
+                    $templateProcessor->setValue('total_transaction#' . ($key + 1), number_format($total, 2));
+                    $totalAll += $total;
+                }
+
+                $templateProcessor->setValue('total', number_format($totalAll, 2));
+
+                $templateProcessor->setValue('date', date('Y-m-d'));
+                $templateProcessor->setValue('user', auth()->user()->name);
+
+
+
+                $filename = 'Sales';
+                $tempPath = 'reports/' . $filename . '.docx';
+
+                if (!file_exists(storage_path('reports'))) {
+                    mkdir(storage_path('reports'), 0777, true);
+                }
+
+                $templateProcessor->saveAs(storage_path($tempPath));
+                return response()->download(storage_path($tempPath));
             }
-
-            $templateProcessor->setValue('total', number_format($totalAll, 2));
-
-
-            $templateProcessor->setValue('date', date('F d, Y', strtotime($this->dateInput)));
-            $templateProcessor->setValue('user', auth()->user()->name);
-
-
-
-            $filename = 'Daily-sales-' . $this->dateInput;
-            $tempPath = 'reports/' . $filename . '.docx';
-
-            if (!file_exists(storage_path('reports'))) {
-                mkdir(storage_path('reports'), 0777, true);
-            }
-
-            $templateProcessor->saveAs(storage_path($tempPath));
-            return response()->download(storage_path($tempPath));
-
-            // session()->flash('message', 'Data exported successfully.');
         }
+        else if($this->selectedDay == "month")
+        {
+            $purchasedProducts = PurchaseProductInfo::with('purchasedProducts')->whereMonth('date_preparation', now('m'))->get();
+
+            if ($purchasedProducts->isEmpty()) {
+                session()->flash('message', 'No records found for the selected date.');
+            } else {
+
+                $count = $purchasedProducts->count();
+
+                if ($count > 0) {
+                    $templateProcessor->cloneRow('reference', $count);
+                }
+
+                $totalAll = 0;
+
+                foreach ($purchasedProducts as $key => $transaction) {
+                    $templateProcessor->setValue('reference#' . ($key + 1), $transaction->reference_no);
+                    $templateProcessor->setValue('date#' . ($key + 1), $transaction->date_preparation->format('Y-m-d'));
+                    $total = $transaction->purchasedProducts->sum('total');
+                    $templateProcessor->setValue('total_transaction#' . ($key + 1), number_format($total, 2));
+                    $totalAll += $total;
+                }
+
+                $templateProcessor->setValue('total', number_format($totalAll, 2));
+
+                $templateProcessor->setValue('date', date('Y-m-d'));
+                $templateProcessor->setValue('user', auth()->user()->name);
+
+
+
+                $filename = 'Sales';
+                $tempPath = 'reports/' . $filename . '.docx';
+
+                if (!file_exists(storage_path('reports'))) {
+                    mkdir(storage_path('reports'), 0777, true);
+                }
+
+                $templateProcessor->saveAs(storage_path($tempPath));
+                return response()->download(storage_path($tempPath));
+            }
+        }
+        else if($this->selectedDay == "year")
+        {
+            $purchasedProducts = PurchaseProductInfo::with('purchasedProducts')->whereYear('date_preparation', now('Y'))->get();
+
+            if ($purchasedProducts->isEmpty()) {
+                session()->flash('message', 'No records found for the selected date.');
+            } else {
+
+                $count = $purchasedProducts->count();
+
+                if ($count > 0) {
+                    $templateProcessor->cloneRow('reference', $count);
+                }
+
+                $totalAll = 0;
+
+                foreach ($purchasedProducts as $key => $transaction) {
+                    $templateProcessor->setValue('reference#' . ($key + 1), $transaction->reference_no);
+                    $templateProcessor->setValue('date#' . ($key + 1), $transaction->date_preparation->format('Y-m-d'));
+                    $total = $transaction->purchasedProducts->sum('total');
+                    $templateProcessor->setValue('total_transaction#' . ($key + 1), number_format($total, 2));
+                    $totalAll += $total;
+                }
+
+                $templateProcessor->setValue('total', number_format($totalAll, 2));
+
+                $templateProcessor->setValue('date', date('Y-m-d'));
+                $templateProcessor->setValue('user', auth()->user()->name);
+
+
+
+                $filename = 'Sales';
+                $tempPath = 'reports/' . $filename . '.docx';
+
+                if (!file_exists(storage_path('reports'))) {
+                    mkdir(storage_path('reports'), 0777, true);
+                }
+
+                $templateProcessor->saveAs(storage_path($tempPath));
+                return response()->download(storage_path($tempPath));
+            }
+        }
+        else
+        {
+            $purchasedProducts = PurchaseProductInfo::with('purchasedProducts')->whereBetween('date_preparation', [$this->from, $this->to])->get();
+
+            if ($purchasedProducts->isEmpty()) {
+                session()->flash('message', 'No records found for the selected date.');
+            } else {
+
+                $count = $purchasedProducts->count();
+
+                if ($count > 0) {
+                    $templateProcessor->cloneRow('reference', $count);
+                }
+
+                $totalAll = 0;
+
+                foreach ($purchasedProducts as $key => $transaction) {
+                    $templateProcessor->setValue('reference#' . ($key + 1), $transaction->reference_no);
+                    $templateProcessor->setValue('date#' . ($key + 1), $transaction->date_preparation->format('Y-m-d'));
+                    $total = $transaction->purchasedProducts->sum('total');
+                    $templateProcessor->setValue('total_transaction#' . ($key + 1), number_format($total, 2));
+                    $totalAll += $total;
+                }
+
+                $templateProcessor->setValue('total', number_format($totalAll, 2));
+
+                $templateProcessor->setValue('date', date('Y-m-d'));
+                $templateProcessor->setValue('user', auth()->user()->name);
+
+                $filename = 'Sales';
+                $tempPath = 'reports/' . $filename . '.docx';
+
+                if (!file_exists(storage_path('reports'))) {
+                    mkdir(storage_path('reports'), 0777, true);
+                }
+
+                $templateProcessor->saveAs(storage_path($tempPath));
+                return response()->download(storage_path($tempPath));
+            }
+        }
+        session()->flash('message', 'Please select a date.');
     }
 }
